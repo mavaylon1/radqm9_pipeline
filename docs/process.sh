d787feda59d470e8417991276ebba50bf5877fb7
Matthew Avaylon <mavaylon1@berkeley.edu>
2024-03-07 16:36:17 -0800

    Update process.sh

--- docs/process.sh
+++ docs/process.sh
@@ -8,13 +8,9 @@
 #SBATCH -c 128
 
 export SLURM_CPU_BIND="cores"
-# srun python ./radqm9_pipeline/src/radqm9_pipeline/processing/process_dataset.py \
+srun python ../src/radqm9_pipeline/processing/process_dataset.py \
 #     --file="path to dataset " \
 #     --forces_key="forces" \
 #     --prefix="<add prefix>" \
 #     --directory='path to directory to store hdf5 files. Do not end with "/"' \
-srun python ../src/radqm9_pipeline/processing/process_dataset.py \
-    --file="/pscratch/sd/m/mavaylon/sam_ldrd/all_clean_charge_full_chunked/charge_spin_test/charge_spin_test_0_1" \
-    --forces_key=" forces" \
-    --prefix="test" \
-    --directory='/pscratch/sd/m/mavaylon/chem_directory' \
+
